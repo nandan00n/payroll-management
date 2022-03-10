7a0a4a7c0b7c83d046166aaaaa1a3811d998b411
Nandan Kumar N <67855274+nandan00n@users.noreply.github.com>
2022-03-10 17:45:57 +0530

    ui and forget password components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import LoginandRegister from "./component/lrc/LoginandRegister";
-import Forgetpassword from './component/forgottenpassword/Forgetpassword';
-import
+// import Forgetpassword from './component/forgottenpassword/Forgetpassword';
+// import { BrowserRouter, Link } from "react-router-dom";
 import './App.css'
 
 function App() {
   return (
     <>
+    {/* <BrowserRouter>
+      <ul>
+          <li>
+            <Link to='login'>Login</Link>
+          </li>
+
+      </ul>
+    
+    <Routes>
+      <Route />
+
+      <Route />
+
+    </Routes>
+    
+    </BrowserRouter> */}
 
       {/* // <Login/> */}
       <LoginandRegister/>
